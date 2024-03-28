6073f4c4dab9b866f283f07bcadc9de376602ccb
Krutonium <PFCKrutonium@gmail.com>
2024-03-28 00:35:15 -0400

    250ms might be a bit tight but it should be fine; we're trying to catch somthing that's intermittant.

--- LeakyPipe/Program.cs
+++ LeakyPipe/Program.cs
@@ -9,23 +9,58 @@ class Program
     static void Main(string[] args)
     {
         // Get all tip IPs in hops from me to 8.8.8.8
+        Console.WriteLine("Getting all IPs in route...");
+        var traceRoute = TraceRoute.GetTraceRoute("8.8.8.8");
+        Dictionary<IPAddress, int> failures = new();
+        Dictionary<IPAddress, int> successes = new();
+        int rounds = 0;
         while (true)
         {
-            var traceRoute = TraceRoute.GetTraceRoute("8.8.8.8");
+            rounds++;
             //Ping each IP to see if it's alive
-            foreach (var ip in traceRoute)
+            Parallel.ForEach(traceRoute,  ip =>
             {
                 var ping = new Ping();
-                var reply = ping.Send(ip);
-                if (reply.Status == IPStatus.Success)
+                var reply = ping.Send(ip, 250);
+                if (reply.Status != IPStatus.Success)
                 {
-                    Console.WriteLine($"Ping to {ip} took {reply.RoundtripTime} ms");
+                    if(!failures.ContainsKey(ip))
+                    {
+                        failures.Add(ip, 1);
+                    }
+                    else
+                    {
+                        failures[ip]++;
+                    }
                 }
                 else
                 {
-                    Console.WriteLine($"Ping to {ip} failed");
+                    if (!successes.ContainsKey(ip))
+                    {
+                        successes.Add(ip, 1);
+                    }
+                    else
+                    {
+                        successes[ip]++;
+                    }
                 }
+            });
+            //Print out the results
+            Console.Clear();
+            Console.WriteLine($"Round {rounds}");
+            Console.WriteLine("Successes:");
+            foreach (var success in successes)
+            {
+                Console.WriteLine($"{success.Key}: {success.Value}");
             }
+            Console.WriteLine("Failures:");
+            foreach (var failure in failures)
+            {
+                Console.WriteLine($"{failure.Key}: {failure.Value}");
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("What you want to look for is the failure that is the big deviator; some nodes just don't respond to pings. If you see a node that is failing a lot more than the others, that's a good indication that it's the problem.");
         }
     }
     public class TraceRoute
@@ -40,7 +75,7 @@ class Program
         {
             Ping pinger = new Ping();
             PingOptions pingerOptions = new PingOptions(ttl, true);
-            int timeout = 10000;
+            int timeout = 1000;
             byte[] buffer = Encoding.ASCII.GetBytes(Data);
             PingReply reply = default(PingReply);
 
@@ -48,8 +83,7 @@ class Program
 
             List<IPAddress> result = new List<IPAddress>();
             if (reply.Status == IPStatus.Success)
-            {
-                Console.WriteLine($"Added {reply.Address}");
+            { 
                 result.Add(reply.Address);
             }
             else if (reply.Status == IPStatus.TtlExpired || reply.Status == IPStatus.TimedOut)
